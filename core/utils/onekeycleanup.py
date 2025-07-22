5e151089414126e678adca04d629edbf8fed7049
lijiaping <gahoo@126.com>
2025-07-22 10:19:44 +0000

    no need video_name any more since we use task for separation;

--- core/utils/onekeycleanup.py
+++ core/utils/onekeycleanup.py
@@ -1,27 +1,20 @@
 import os
 import glob
-from core._1_ytdlp import find_video_files
 import shutil
+from core._1_ytdlp import find_media_file
 
 def cleanup(history_dir="history"):
-    # Get video file name
-    video_file = find_video_files()
-    video_name = video_file.split("/")[1]
-    video_name = os.path.splitext(video_name)[0]
-    video_name = sanitize_filename(video_name)
-    
     # Create required folders
     os.makedirs(history_dir, exist_ok=True)
-    video_history_dir = os.path.join(history_dir, video_name)
-    log_dir = os.path.join(video_history_dir, "log")
-    gpt_log_dir = os.path.join(video_history_dir, "gpt_log")
+    log_dir = os.path.join(history_dir, "log")
+    gpt_log_dir = os.path.join(history_dir, "gpt_log")
     os.makedirs(log_dir, exist_ok=True)
     os.makedirs(gpt_log_dir, exist_ok=True)
 
     # Move non-log files
     for file in glob.glob("output/*"):
         if not file.endswith(('log', 'gpt_log')):
-            move_file(file, video_history_dir)
+            move_file(file, history_dir)
 
     # Move log files
     for file in glob.glob("output/log/*"):
